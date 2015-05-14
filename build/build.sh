186582cfb7afd158713b04f333a4cc9874be8110
Max Rozdobudko <max.rozdobudko@gmail.com>
2015-05-14 16:43:13 +0300

    updates legacy build script

--- build/build.sh
+++ build/build.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-unzip -o card.io-ane.swc
+unzip -o card.io.swc
 
-unzip -o default/card.io-ane-default.swc -d default
+unzip -o default/card.io-default.swc -d default
 
 # copy library to be included into ANE
 mkdir -p ios
 cp -R -L -f ../card.io-ane-ios/CardIO/Pods/CardIO/CardIO/libCardIO.a ios/libCardIO.a
 
-adt -package -storetype pkcs12 -keystore $AIR_CERTIFICATE -storepass $AIR_CERTIFICATE_STOREPASS -target ane card.io.ane extension.xml -swc card.io-ane.swc -platform iPhone-ARM library.swf -platformoptions platform-ios.xml -C ios . -platform default -C default library.swf
+adt -package -storetype pkcs12 -keystore $AIR_CERTIFICATE -storepass $AIR_CERTIFICATE_STOREPASS -target ane card.io.ane extension.xml -swc card.io.swc -platform iPhone-ARM library.swf -platformoptions platform-ios.xml -C ios . -platform default -C default library.swf
 
 #copy ANE into bin directory
 cp -R card.io.ane ../bin/card.io.ane
 
 # copy ANE for debug project
-cp -R card.io.ane ../card.io-ane-air/card.io-ane-air-debug/ane/card.io.ane
+cp -R card.io.ane ../card.io-ane-air/card.io-debug/ane/card.io.ane
 
 mkdir -p launch/ext
 cp -R card.io.ane launch/ext/card.io.ane
